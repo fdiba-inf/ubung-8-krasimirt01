14442ce8a3794ad6cd9b2316e2bd18388be522ff
krasimirt01 <krasimir.tashkov@fdiba.tu-sofia.bg>
2020-11-23 14:48:50 +0000

    -

--- src/exercise8/Point.java
+++ src/exercise8/Point.java
@@ -1,40 +1,47 @@
 package exercise8;
 
 public class Point {
-    // Define attributes
+  private double x;
+  private double y;
 
-    public Point() {
-        // Initialize attributes to 0
-    }
+  public Point() {
+    x = 0;
+    y = 0;
+  }
 
-    public Point(double x, double y) {
-        // Initialize attributes by coping x and y parameters
-    }
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    public Point(Point otherPoint) {
-        // Initialize attributes by coping attributes of otherPoint
-    }
+  public Point(Point otherPoint) {
+    x = otherPoint.x;
+    y = otherPoint.y;
+  }
 
-    public void initialize() {
-        // Change attributes through console
-    }
+  public void initialize() {
+    x = Utils.INPUT.nextDouble();
+    y = Utils.INPUT.nextDouble();
+  }
 
-    public void translate(double xDelta, double yDelta) {
-        // Change attributes by adding xDelta and yDelta
-    }
+  public void translate(double xDelta, double yDelta) {
+    x += xDelta;
+    y += yDelta;
+  }
 
-    public Point createNewTranslatedPoint(double xDelta, double yDelta) {
-        // Create new point which is translated relative to this point
-        return null;
-    }
+  public Point createNewTranslatedPoint(double xDelta, double yDelta) {
+    double xnew = x + xDelta;
+    double ynew = y + yDelta;
+    return new Point(xnew, ynew);
+  }
 
-    public boolean equals(Point otherPoint) {
-        // Compare attributes of this point to attributes of otherPoint for equality
-        return false;
-    }
+  public boolean equals(Point otherPoint) {
+    boolean pruf1 = Utils.equals(x, otherPoint.x);
+    boolean pruf2 = Utils.equals(y, otherPoint.y);
+    return pruf1 && pruf2;
+  }
 
-    public String toString() {
-        // Represent attributes as string
-        return null;
-    }
+  public String toString() {
+    return "(" + x + ", " + y + ")";
+  }
 }
